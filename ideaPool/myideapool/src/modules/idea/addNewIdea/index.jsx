dcf39f174a979446c61402f7dc9615edc982082f
nburd01 <n.burdon@protonmail.com>
2022-12-17 17:18:59 +0100

    finishing redux bases

--- ideaPool/myideapool/src/modules/idea/addNewIdea/index.jsx
+++ ideaPool/myideapool/src/modules/idea/addNewIdea/index.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
-
+import { useDispatch } from 'react-redux'
+import { addIdea } from '../../../store/ideaSlice';
 export const AddNewIdea = () => {
+
+    const disptach = useDispatch();
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        disptach(
+            addIdea({
+                title:value,
+        }))
+    }
+
   return (
     <div>AddNewIdea</div>
   )
