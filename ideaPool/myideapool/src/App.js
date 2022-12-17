841d581d3954d444c5febcc600489b46c2ad1747
nburd01 <n.burdon@protonmail.com>
2022-12-17 16:39:25 +0100

    all the way to IdeaList

--- ideaPool/myideapool/src/App.js
+++ ideaPool/myideapool/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import { Routes } from './routes';
 
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>My idea Pool</h1>
+      </div>
+      <div>
+        <Routes/>
+      </div>
     </div>
   );
 }
---
+++ ideaPool/myideapool/src/modules/idea/ideaPage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Empty } from '../empty'
+import { IdeaList } from '../ideaList'
+
+export const IdeaPage = () => {
+  return (
+    <div>
+        <IdeaList/>
+        <Empty/>
+    </div>
+  )
+}
