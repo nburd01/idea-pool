5009103c8bb03e6d06883be217f579a8087ad027
nburd01 <n.burdon@protonmail.com>
2022-12-17 17:24:17 +0100

    payload functioning

--- ideaPool/myideapool/src/modules/idea/addNewIdea/index.jsx
+++ ideaPool/myideapool/src/modules/idea/addNewIdea/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addIdea } from '../../../store/ideaSlice';
 export const AddNewIdea = () => {
-
+	const [value, setValue] = useState('');
     const disptach = useDispatch();
 
     const onSubmit = (event) => {
@@ -13,7 +13,20 @@ export const AddNewIdea = () => {
         }))
     }
 
-  return (
-    <div>AddNewIdea</div>
-  )
+    return (
+		<form onSubmit={onSubmit} className='form-inline mt-3 mb-3'>
+			<label className='sr-only'>Name</label>
+			<input
+				type='text'
+				className='form-control mb-2 mr-sm-2'
+				placeholder='Add todo...'
+				value={value}
+				onChange={(event) => setValue(event.target.value)}
+			></input>
+
+			<button type='submit' className='btn btn-primary mb-2'>
+				Submit
+			</button>
+		</form>
+	);
 }
