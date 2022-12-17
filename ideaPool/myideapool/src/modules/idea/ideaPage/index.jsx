36a7f4669c875afb314a2d1a978207b11e0f7955
nburd01 <n.burdon@protonmail.com>
2022-12-17 16:43:44 +0100

    added preRedux

--- ideaPool/myideapool/src/modules/idea/ideaPage/index.jsx
+++ ideaPool/myideapool/src/modules/idea/ideaPage/index.jsx
@@ -3,6 +3,17 @@ import { Empty } from '../empty'
 import { IdeaList } from '../ideaList'
 
 export const IdeaPage = () => {
+
+    const handleAddNewIdea = () => {
+        if (idea) return;
+        dispatch(addNewIdea({
+          content: '',
+          impact: 10,
+          ease: 10,
+          confidence: 10,
+        }));
+      };
+
   return (
     <div>
         <IdeaList/>
